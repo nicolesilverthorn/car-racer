210fd97012004fbe191b5832a78050b5eccdec7e
nicolesilverthorn <nicolesilverthorn@hotmail.com>
2024-07-01 11:55:33 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -22,9 +22,46 @@ let player = { speed: 7, score: 0 };
 level.addEventListener('click', (e)=> {
     player.speed = levelSpeed[e.target.id];
 });
+level.addEventListener('touchstart', (e)=> {
+    player.speed = levelSpeed[e.target.id];
+});
 
 startScreen.addEventListener('click', () => {
-    // gameArea.classList.remove('hide');
+    startScreen.classList.add('hide');
+    gameArea.innerHTML = "";
+
+    player.start = true;
+    gameStart.play();
+    gameStart.loop = true;
+    player.score = 0;
+    window.requestAnimationFrame(gamePlay);
+
+    for(let i=0; i<5; i++){
+        let roadLineElement = document.createElement('div');
+        roadLineElement.setAttribute('class', 'roadLines');
+        roadLineElement.y = (i*150);
+        roadLineElement.style.top = roadLineElement.y + "px";
+        gameArea.appendChild(roadLineElement);
+    }
+
+    let carElement = document.createElement('div');
+    carElement.setAttribute('class', 'car');
+    gameArea.appendChild(carElement);
+
+    player.x = carElement.offsetLeft;
+    player.y = carElement.offsetTop  ;
+
+    for(let i=0; i<3; i++){
+        let enemyCar = document.createElement('div');
+        enemyCar.setAttribute('class', 'enemyCar');
+        enemyCar.y = ((i+1) * 350) * - 1;
+        enemyCar.style.top = enemyCar.y + "px";
+        enemyCar.style.backgroundColor = randomColor();
+        enemyCar.style.left = Math.floor(Math.random() * 350) + "px";
+        gameArea.appendChild(enemyCar);
+    }
+});
+startScreen.addEventListener('touchstart', () => {
     startScreen.classList.add('hide');
     gameArea.innerHTML = "";
 
